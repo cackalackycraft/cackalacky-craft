cf6c9111f774736e409b6ed75e713d1bc044a5e3
Jeffrey A. Bunch <jeffreya.bunch@Mac-mini.local>
2026-05-24 08:34:45 -0400

    Update astro config for Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://cackalackycraft.com',
-  integrations: [],
-
-  vite: {
-    plugins: [tailwindcss()],
-    css: {
-      devSourcemap: true,
-    },
-  },
-
-  adapter: cloudflare(),
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+  }),
+  site: 'https://cackalacky-craft.pages.dev',
 });
